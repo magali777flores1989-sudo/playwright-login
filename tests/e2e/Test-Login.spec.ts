063e988ef7b4640b2c834bb28a167b11ce858f9f
Magali Flores <magali777flores1989@gmail.com>
2026-02-15 00:31:10 +0100

    fix: prevent undefined values in login test

--- tests/e2e/Test-Login.spec.ts
+++ tests/e2e/Test-Login.spec.ts
@@ -1,23 +1,52 @@
-import { test} from '@playwright/test';
-import {  LoginPage} from '../../src/pages/login.page';
+import { expect, test } from '@playwright/test';
+import { LoginPage } from '../../src/pages/login.page';
 import { BasePage } from '../../src/pages/basePage';
 import { createUser } from '../../src/data/user';
 
+const user = process.env.LOGIN_EMAIL!
+const password = process.env.LOGIN_PASSWORD!
 
-test('Test de registro', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  const base = new BasePage(page);
-  const user = createUser();
-
+test.describe('Test de Login', () => {
 
 
-  await base.goWeb('https://demowebshop.tricentis.com/');
-  await loginPage.goLogin();
-  await loginPage.login(
-    process.env.LOGIN_EMAIL!,
-    process.env.LOGIN_PASSWORD! 
-  );
- 
+
+  test.beforeEach(async ({ page }) => {
+
+    const loginPage = new LoginPage(page);
+    await page.goto('https://demowebshop.tricentis.com/');
+
+    await loginPage.goLogin();
+    await loginPage.login(
+      user,
+      password
+    );
+  });
+
+
+  test('Test de Login exitoso', async ({ page }) => {
+
+    expect(page.getByText('nadia123@gmail.com')).toBeVisible();
+
+  });
+
+
+  test('Test de comprobar boton Log out', async ({ page }) => {
+
+    expect(page.getByText('Log out')).toBeVisible();
+
+
+  });
+
+
+test('Test de comprobar elemento', async ({ page }) => {
+
+    expect(page.locator("#topcartlink")).toBeVisible();
+
+
+  });
+
 });
+
+
 
 
